dab71e62913cd6422125f4c53c4d09b2eb671100
Jared Tichacek <jared.tichacek@gmail.com>
2023-04-22 17:17:11 -0500

    added PUT method for IndexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,20 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.log('PUT to the db')
+  // create connection to db
+  const jateDb = await openDB('jate', 1);
+  // create transaction
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // create object store
+  const store = tx.objectStore('jate');
+  // use put method to update content in db
+  const request = store.put({ id, content });
+  // wait for transaction to complete
+  const result = await request;
+  console.log('Data saved to db', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
